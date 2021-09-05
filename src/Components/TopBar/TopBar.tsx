dc0c430cce94a1af9a3a1675134cca98ec353adc
EmIT <emad.armoun@gmail.com>
2021-09-06 02:03:52 +0430

    refactor: move TopBar styles to a separate file + add TopBar props types

--- src/Components/TopBar/TopBar.tsx
+++ src/Components/TopBar/TopBar.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { AppBar, Avatar, Grid, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Avatar, Grid, IconButton, Toolbar, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import TopBarPropTypes from './TopBarPropTypes';
+import useStyles from './TopBarStyles';
 
-const useStyles = makeStyles(theme => ({
-    white: {
-      backgroundColor: '#FFF'
-    }
-}))
-
-export function TopBar() {
+export function TopBar(props: TopBarPropTypes) {
   const classes = useStyles();
+  const {openDrawer} = props;
 
   return (
     <>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => openDrawer()}>
             <MenuIcon />
           </IconButton>
 
---
+++ src/Components/TopBar/TopBarStyles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    white: {
+      backgroundColor: '#FFF'
+    }
+}))
+
+export default useStyles;
