906985c7601ba9bc6a828c3d911abd2f9805383f
EmIT <emad.armoun@gmail.com>
2021-09-05 22:26:33 +0430

    feat: install material-ui & initalize theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from '@material-ui/styles';
+import { createTheme, responsiveFontSizes } from '@material-ui/core';
+import { orange, purple } from '@material-ui/core/colors';
+
+const theme = responsiveFontSizes(createTheme({
+  typography: {
+    fontSize: 12,
+  },
+  palette: {
+    primary: {
+      main: purple[500]
+    },
+    secondary: {
+      main: orange[400]
+    }
+  }
+}));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
