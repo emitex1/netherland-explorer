50aa4e614b7387b127ba2e915f1624beb01341c9
EmIT <emad.armoun@gmail.com>
2021-09-06 00:48:26 +0430

    feat: add AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
-import { Container, CssBaseline, Grid, Typography } from '@material-ui/core';
+import { AppBar, Avatar, Container, CssBaseline, Grid, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  white: {
+    backgroundColor: '#FFF'
+  }
+}))
 
 function App() {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <CssBaseline />
+
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Grid container alignItems="center" spacing={2}>
+            <Grid item>
+              <Avatar alt="Logo" src="favicon-32x32.png" className={classes.white} />
+            </Grid>
+            <Grid item alignItems="center">
+              <Typography variant="h6" color="inherit" align="center">
+                Netherland Explorer
+              </Typography>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
 
       <Container>
         <Grid container spacing={3}>
