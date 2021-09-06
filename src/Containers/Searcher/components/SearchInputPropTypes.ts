2bfbb838fd7623f78eba514760ae33eda4ae3e02
Seyed Emad Armoun <Emad.Armoun@gmail.com>
2021-09-06 17:32:48 +0430

    initial commit

--- src/Containers/Searcher/components/SearchInputPropTypes.ts
+++
@@ -1,3 +0,0 @@
-export default interface SearchInputPropTypes {
-  doSearch: (searchKeyword: string) => void
-}
--- src/Util/formatter.ts
+++
@@ -1,3 +0,0 @@
-export const numberWithCommas = (x: any) => {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
