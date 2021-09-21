337f4b4fcecf584583d7d63033ea037f23907175
EmIT <emad.armoun@gmail.com>
2021-09-21 08:23:29 +0430

    feat: improve alignment of About page in mob size

--- src/Containers/AboutMe/AboutMe.tsx
+++ src/Containers/AboutMe/AboutMe.tsx
@@ -1,36 +1,40 @@
-import { ButtonBase, Grid, Link, Paper, Typography } from "@material-ui/core";
+import { ButtonBase, Grid, Link, Paper, Typography, useMediaQuery, useTheme } from "@material-ui/core";
 import useStyles from "./AboutMeStyles";
 import mePhoto from "../../assets/images/me.jpg";
 
 export function AboutMe() {
   const classes = useStyles();
 
+  const theme = useTheme();
+  const mobileMatches = useMediaQuery(theme.breakpoints.down('xs'));
+  const properAlign = mobileMatches ? "center" : undefined;
+
   return (
     <Paper className={classes.paper}>
       <Grid container spacing={4}>
 
-        <Grid item>
+        <Grid item xs={12} sm={4} container justifyContent={properAlign}>
           <ButtonBase className={classes.image}>
             <img className={classes.img} alt="Emad" src={mePhoto} />
           </ButtonBase>
         </Grid>
 
         
-        <Grid item xs={12} sm container>
+        <Grid item xs={12} sm={8} container>
           <Grid item xs container direction="column" spacing={2}>
 
-            <Typography gutterBottom variant="h3">
+            <Typography gutterBottom variant="h3" align={properAlign}>
               Emad Armoun
             </Typography>
-            <Typography variant="h5" gutterBottom>
+            <Typography variant="h5" gutterBottom align={properAlign}>
               Full-stack JavaScript Developer
             </Typography>
-            <Typography variant="body1" color="textSecondary">
+            <Typography variant="body1" color="textSecondary" align={properAlign}>
               <Link href="http://www.EmadArmoun.com" target="_blank">
                 www.EmadArmoun.com
               </Link>
             </Typography>
-            <Typography variant="body1" color="textSecondary">
+            <Typography variant="body1" color="textSecondary" align={properAlign}>
               <Link href="mailto:Emad.Armoun@gmail.com">
                 Emad.Armoun@gmail.com
               </Link>
