ff8bbcf59fa7b51a455968710a116e146fbcce4f
EmIT <emad.armoun@gmail.com>
2021-09-21 07:45:13 +0430

    feat: hide burgur menu in mobile size

--- src/Components/TopBar/TopBar.tsx
+++ src/Components/TopBar/TopBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Avatar, Grid, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Avatar, Grid, Hidden, IconButton, Toolbar, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import TopBarPropTypes from './TopBarPropTypes';
 import useStyles from './TopBarStyles';
@@ -13,9 +13,12 @@ export function TopBar(props: TopBarPropTypes) {
     <>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => openDrawer()}>
-            <MenuIcon />
-          </IconButton>
+
+          <Hidden xsDown>
+            <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => openDrawer()}>
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
 
           <Grid container alignItems="center" spacing={2}>
 
